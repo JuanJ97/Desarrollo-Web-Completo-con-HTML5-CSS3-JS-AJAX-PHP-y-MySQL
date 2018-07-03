497cb19dde8011a4cb37e09be5b7c66a80a49834
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-07-02 23:17:21 -0500

    Emepezando JS de reservaciones

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function(){
+    "use strict";
+    document.addEventListener('DOMContentLoaded', function(){
+       var nombre = document.getElementById('nombre');
+       var apellido = document.getElementById('apellido');
+       var email = document.getElementById('email');
+        var pase_dia = document.getElementById('pase_un_dia');
+        var pase_dos_dias = document.getElementById('pase_dos_dias');
+        var pase_todos_dias = document.getElementById('pase_todos_dias');
+        
+    });
+})();
