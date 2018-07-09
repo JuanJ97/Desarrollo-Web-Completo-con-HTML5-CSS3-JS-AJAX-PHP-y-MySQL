49511df2af45f88e63c3d59d5e81c4b3253a2cdc
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-07-09 01:51:39 -0500

    Agregando jquery al programa de eventos

--- js/main.js
+++ js/main.js
@@ -103,3 +103,20 @@
         }
     });
 })();
+
+$(function() {
+  $('div.ocultar').hide();
+  $('.programa-evento .info-cursos:first').show();
+  $('.menu-programa a:first').addClass('activo');
+
+  $('.menu-programa a').on('click', function(){
+    $('.menu-programa a').removeClass('activo');
+    $(this).addClass('activo');
+    $('.ocultar').hide();
+
+    var enlace = $(this).attr('href');
+    $(enlace).fadeIn(1000);
+    return false;
+  });
+});
+
