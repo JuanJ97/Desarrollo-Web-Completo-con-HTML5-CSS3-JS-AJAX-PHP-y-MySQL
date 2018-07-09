cffcad06a5577c5553df8703853af58c9fdf87f5
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-07-09 02:49:38 -0500

    Animacion de contador con jquery

--- js/main.js
+++ js/main.js
@@ -118,5 +118,9 @@ $(function() {
     $(enlace).fadeIn(1000);
     return false;
   });
+    $('.resumen-evento li:nth-child(1) p').animateNumber({number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({number: 3}, 1200);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({number: 9}, 1200);
 });
 
