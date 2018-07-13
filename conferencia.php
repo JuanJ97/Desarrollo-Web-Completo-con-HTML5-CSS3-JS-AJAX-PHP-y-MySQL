7770486fb9766ecd038e4b720c6635f859dcfe39
Juan Jesus Morales Ortiz <juanjosemorales2a@gmail.com>
2018-07-13 02:40:02 -0500

    MIgrando plataforma a PHP y creando MVC

---
+++ conferencia.php
@@ -0,0 +1,23 @@
+<?php include_once 'php/header.php'; ?>
+ <section class="seccion contenedor">
+     <h2>La mejor conferencia de diseño web en español</h2>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia fuga aperiam natus aliquid suscipit cum provident nulla ullam architecto, eos consectetur explicabo quaerat optio culpa, vitae libero nam, accusantium quae.</p>
+ </section>
+<section class="seccion contenedor">
+    <h2>Galeria de fotos</h2>
+    <div class="galeria">
+    <a href="img/galeria/01.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/01.jpg" alt=""></a>
+    <a href="img/galeria/02.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/02.jpg" alt=""></a>
+    <a href="img/galeria/03.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/03.jpg" alt=""></a>
+    <a href="img/galeria/04.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/04.jpg" alt=""></a>
+    <a href="img/galeria/05.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/05.jpg" alt=""></a>
+    <a href="img/galeria/06.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/06.jpg" alt=""></a>
+    <a href="img/galeria/07.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/07.jpg" alt=""></a>
+    <a href="img/galeria/08.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/08.jpg" alt=""></a>
+    <a href="img/galeria/09.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/09.jpg" alt=""></a>
+    <a href="img/galeria/10.jpg" data-lightbox="galeria"><img src="img/galeria/thumbs/10.jpg" alt=""></a>
+    </div>
+</section>
+ 
+ 
+ <?php include_once 'php/footer.php'; ?>
---
+++ php/header.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>GDL WEB CAMP</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+<header class="site-header">
+    <div class="hero">
+        <div class="contenido-header">
+            <nav class="redes-sociales">
+                <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href=""><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                <a href=""><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            </nav>
+            <div class="informacion-evento">
+               <div class="clearfix">
+                    <p class="fecha"><i class="fa fa-calendar" aria-hidden="true"></i> 10-12 DIC</p>
+                <p class="ciudad"><i class="fa fa-map-marker" aria-hidden="true"></i> Guadalajara, MX</p>
+               </div>
+               
+                <h1 class="nombre-sitio">GdlWebCamp</h1>
+            <p class="slogan">La mejor conferencia de <span>diseño web</span></p>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="barra">
+    <div class="contenedor clearfix">
+        <div class="logo"><a href="index.php"><img src="img/logo.svg" alt="GDLWEBCAMP-logo"></a></div>
+        <div class="menu-movil">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <nav class="navegacion-principal clearfix">
+            <a href="conferencia.php">Conferencia</a>
+            <a href="">Calendario</a>
+            <a href="">Invitados</a>
+            <a href="registro.php">Reservaciones</a>
+        </nav>
+    </div>
+</div>
